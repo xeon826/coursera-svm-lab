7829aaa0b87f54f737629352571335c2bbc1a470
Joshua Wilkeson <xeon826@gmail.com>
2024-10-22 19:08:34 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pylab as pl
+import numpy as np
+import scipy.optimize as opt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+cell_df = pd.read_csv('cell_samples.csv')
+# print(cell_df.head())
+ax = cell_df[cell_df['Class'] == 4][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='DarkBlue', label='malignant');
+cell_df[cell_df['Class'] == 2][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='Yellow', label='benign', ax=ax);
+# plt.show()
+print(cell_df.dtypes)
